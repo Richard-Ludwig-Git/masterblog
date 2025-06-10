e379a2baf2ec30712a922987a76114996ab46b42
Vladtepesch <vladtepesch@Mac.fritz.box>
2025-06-10 20:11:28 +0200

    Working blog with delete, update and add buttons

--- app.py
+++ app.py
@@ -1,10 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import redirect
+import json
+import uuid
 
 app = Flask(__name__)
 
+def fetch_post_by_id(post_id):
+    with open("data.json", "r") as grap_it:
+        posts = json.load(grap_it)
+        for post in posts:
+            if post["id"] == post_id:
+                return post
+        return None
+
+
 @app.route('/')
 def index():
-    return render_template("index.html", posts=blog_posts)
+    with open("data.json", "r") as grap_it:
+        blog = json.load(grap_it)
+    return render_template("index.html", posts=blog, title_blog="Was so passiert im Hause Pape-Ludwig")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_post = {}
+        new_post["id"] = str(uuid.uuid4())
+        new_post["title"] = request.form.get("title")
+        new_post["author"] = request.form.get("author")
+        new_post["content"] = request.form.get("content")
+        with open("data.json", "r") as grap_it:
+            d = json.load(grap_it)
+            d.append(new_post)
+        with open("data.json", "w") as dump_it:
+            json.dump(d, dump_it)
+        return redirect(url_for("index"))
+    return render_template("add.html")
+
+@app.route("/delete/<post_id>")
+def delete(post_id):
+    with open("data.json", "r") as grap_it:
+        posts = json.load(grap_it)
+        post = fetch_post_by_id(post_id)
+        if post is None:
+            return "Post not found", 404
+        posts.remove(post)
+        with open("data.json", "w") as dump_it:
+            json.dump(posts, dump_it)
+        return redirect(url_for("index"))
+
+
+@app.route("/update/<post_id>", methods=["GET", "POST"])
+def update(post_id):
+    with open("data.json", "r") as grap_it:
+        posts = json.load(grap_it)
+        post = fetch_post_by_id(post_id)
+        index = posts.index(post)
+
+    if post is None:
+        return "Post not found", 404
+    if request.method == "POST":
+        posts[index]["title"] = request.form.get("title")
+        posts[index]["author"] = request.form.get("author")
+        posts[index]["content"] = request.form.get("content")
+        with open("data.json", "w") as dump_it:
+            json.dump(posts, dump_it)
+            return redirect(url_for("index"))
+    else:
+        return render_template("update.html", post=post)
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
